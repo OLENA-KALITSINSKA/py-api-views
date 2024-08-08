38ccedbf1779d7d58b1fcc54ae5ee68d6cd34ee5
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2024-08-08 14:42:35 +0300

    fixed

--- cinema/views.py
+++ cinema/views.py
@@ -63,12 +63,6 @@ class ActorList(
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
-    def get(self, request, *args, **kwargs) -> Response:
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs) -> Response:
-        return self.create(request, *args, **kwargs)
-
 
 class ActorDetail(
     mixins.RetrieveModelMixin,
@@ -78,18 +72,6 @@ class ActorDetail(
 ):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
-
-    def get(self, request, *args, **kwargs) -> Response:
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs) -> Response:
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs) -> Response:
-        return self.destroy(request, *args, **kwargs)
-
-    def patch(self, request, *args, **kwargs) -> Response:
-        return self.partial_update(request, *args, **kwargs)
 
 
 class CinemaHallViewSet(
